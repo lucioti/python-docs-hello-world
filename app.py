a342077d8cae6b61ba48e37d8c9adef8e1d53425
lucioti <lucioti@microsoft.com>
2021-05-12 12:02:12 -0700

    Customized app, added VS support

--- app.py
+++ app.py
@@ -3,4 +3,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return "Hello, Lucio!"
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
